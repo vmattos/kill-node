b61f7b2d2bcd288902000f7e62cd3e287a604f79
Vitor Mattos <vitor.cavalcanti@olxbr.com>
2017-09-14 11:35:55 -0300

    Plugin stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+kill-node
+=========
+
+Self explanatory
+
+## Usage
+
+```
+$ kill-node # Kills every node proc
+$ kill-node some-buggy-project # Makes little some-buggy-project-jr a dead orphan piece of software :(
+```
---
+++ kill-node.plugin.zsh
@@ -0,0 +1,9 @@
+function kill-node {
+  NODE_PROCS=`ps -ax | grep node`
+  if [ $# -ne 0 ]; then
+    echo "Killing $1 whole family"
+    NODE_PROCS=`echo $NODE_PROCS | grep $1`
+  fi
+  echo "Killing every node processes. Not a single child will survive"
+  kill -15 `echo $NODE_PROCS | awk '{print $1}'`
+}
